3d2df9e22cefadea7085242702151cf0ba082611
Ashok812 <chhetriarash81@gmail.com>
2021-05-28 11:14:26 +0545

    Player State Change Created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React from 'react'
+import React , {useState, useEffect}from 'react'
 import './App.css'
 import Square from './Square'
 
 function App() {
+    const [board, setBoard] = useState(["","","","","","","","","",]);
+    const [player, setPlayer] = useState("X");
+    const chooseSquare = (square) => {
+        setBoard(board.map((value, index) => {
+            if(index === square && value===""){
+                return player
+            }
+            return value;
+        }))
+
+        if(player === "X"){
+            setPlayer("O")
+        }
+        else{
+            setPlayer("X")
+        }
+    }
+    //const 
     return (
         <div className="App">
                 <div className="board">
                     <div className="row">
-                            <Square />
-                            <Square />
-                            <Square />
+                            <Square val = {board[0]} chooseSquare={ () => chooseSquare(0) } />
+                            <Square val = {board[1]} chooseSquare={ () =>chooseSquare(1)}  />
+                            <Square val = {board[2]} chooseSquare={ () => chooseSquare(2) }  />
                     </div>
 
                     <div className="row">
-                            <Square />
-                            <Square />
-                            <Square />
+                                <Square val = {board[3]} chooseSquare={ () => chooseSquare(3) }  />
+                                <Square val = {board[4]} chooseSquare={ () => chooseSquare(4) }  />
+                                <Square val = {board[5]} chooseSquare={ () => chooseSquare(5) }  />
                     </div>
 
                     <div className="row">
-                            <Square />
-                            <Square />
-                            <Square />
+                            <Square val = {board[6]} chooseSquare={ () => chooseSquare(6) }  />
+                            <Square val = {board[7]} chooseSquare={ () => chooseSquare(7) }  />
+                            <Square val = {board[8]} chooseSquare={ () => chooseSquare(8) }  />
                     </div>
                 </div>
         </div>
