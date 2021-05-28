7127131b175a0d308c3792ad5e01d1b418260222
Ashok812 <chhetriarash81@gmail.com>
2021-05-28 10:16:20 +0545

    Square Component and Styling Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Square from './Square'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+                <div className="board">
+                    <div className="row">
+                            <Square />
+                            <Square />
+                            <Square />
+                    </div>
+
+                    <div className="row">
+                            <Square />
+                            <Square />
+                            <Square />
+                    </div>
+
+                    <div className="row">
+                            <Square />
+                            <Square />
+                            <Square />
+                    </div>
+                </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
